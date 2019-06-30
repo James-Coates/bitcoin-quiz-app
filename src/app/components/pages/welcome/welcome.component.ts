c86cd51605ce36112efbc763a734c428625bbd33
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-30 12:29:17 +0100

    initial commit

---
+++ src/app/components/pages/welcome/welcome.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { QuestionsService } from '../../../services/questions.service';
+import { Quiz } from 'src/app/models/quiz.model';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+  private quizList: Quiz[];
+
+  constructor(private questionsService: QuestionsService) {
+    this.questionsService.getQuizzes().subscribe(data => {
+      console.log(data);
+      this.quizList = data;
+    });
+  }
+
+  ngOnInit() {}
+}
