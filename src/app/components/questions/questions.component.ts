7ec0938dd8a6c65db3bfd5daadaa69ff03305dc1
James Coates <James@Lydias-MacBook-Pro.local>
2019-06-30 18:30:18 +0100

    add initial question component logic

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Quiz, Question, Answers, Choice } from '../../models/quiz.model';
+import { QuestionsService } from '../../services/questions.service';
 
 @Component({
   selector: 'app-questions',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questions.component.scss']
 })
 export class QuestionsComponent implements OnInit {
+  private answers: Answers;
+  private quiz: Quiz;
+  private questions: Question[];
+  private currentQuestionIndex: number;
+  private showResults = false;
 
-  constructor() { }
+  // Inject the active route and questions service
+  constructor(
+    private route: ActivatedRoute,
+    private questionsService: QuestionsService
+  ) {}
 
   ngOnInit() {
+    this.questionsService
+      .getQuestions(this.route.snapshot.params.quizId)
+      .subscribe(questions => {
+        this.questions = questions;
+        this.answers = new Answers();
+        this.currentQuestionIndex = 0;
+      });
   }
 
+  updateChoice(choice: Choice) {
+    this.answers.values[this.currentQuestionIndex] = choice;
+  }
+
+  nextOrViewResults() {
+    if (this.currentQuestionIndex === this.questions.length - 1) {
+      this.showResults = true;
+      return;
+    }
+    this.currentQuestionIndex++;
+  }
+
+  reset() {
+    this.quiz = undefined;
+    this.questions = undefined;
+    this.answers = undefined;
+    this.currentQuestionIndex = undefined;
+  }
 }
