b93a88e7c5e66bbb68bc1e49b239f4bc7ddb0913
James Coates <James@Lydias-MacBook-Pro.local>
2019-07-02 20:19:33 +0100

    add basic styling and combine questionAnswers

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Answers } from '../../models/quiz.model';
+import { Answers, Question } from '../../models/quiz.model';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-results',
@@ -8,8 +10,26 @@ import { Answers } from '../../models/quiz.model';
 })
 export class ResultsComponent implements OnInit {
   @Input() answers: Answers;
+  @Input() questions: Question;
+  questionAnswers = [];
+  faCheckCircle = faCheckCircle;
+  faTimesCircle = faTimesCircle;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.questions);
+    console.log(this.answers);
+
+    // for (let i = 0; i < this.questions.keys.length; i++) {
+    //   this.questionAnswers.push({ q: this.questions[i], a: this.answers.values[i] });
+    // }
+
+    for (const index in this.questions) {
+      if (this.questions.hasOwnProperty(index)) {
+        this.questionAnswers.push({ q: this.questions[index].label, a: this.answers.values[index] });
+      }
+    }
+    console.log(this.questionAnswers);
+  }
 }
