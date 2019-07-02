f47001e8838ce8b723f6c7f4e729bb6cd3a0b5f7
James Coates <James@Lydias-MacBook-Pro.local>
2019-07-02 20:03:12 +0100

    add bootstrap, header and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,43 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModule, NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './components/question-form/question-form.component';
 import { ResultsComponent } from './components/results/results.component';
 import { WelcomeComponent } from './components/pages/welcome/welcome.component';
 import { QuestionsComponent } from './components/questions/questions.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
+  { path: 'about', component: AboutComponent },
   { path: ':quizId', component: QuestionsComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' }
 ];
 
 @NgModule({
-  declarations: [AppComponent, QuestionFormComponent, ResultsComponent, WelcomeComponent, QuestionsComponent],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes)],
+  declarations: [
+    AppComponent,
+    QuestionFormComponent,
+    ResultsComponent,
+    WelcomeComponent,
+    QuestionsComponent,
+    HeaderComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    NgbModule,
+    NgbCollapseModule,
+    FontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
